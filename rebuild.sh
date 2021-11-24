43a879490a6c364349198d4c9a952519697d7f97
Mitchell Tishmack <git@mitchty.net>
2021-11-24 17:00:59 -0600

    Add flake.lock and some helper scripts to use things with

---
+++ rebuild.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+#-*-mode: Shell-script; coding: utf-8;-*-
+# File: rebuild.sh
+_base=$(basename "$0")
+_dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P || exit 126)
+export _base _dir
+
+cd "${_dir}" || exit 126
+
+# Don't sudo when we don't need it sudo as root is... rather sad
+as_root() {
+  if [ "root" = "$(id -un)" ]; then
+    "$@"
+  else
+    sudo "$@"
+  fi
+}
+
+as_root nixos-rebuild switch --flake .#
---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+#-*-mode: Shell-script; coding: utf-8;-*-
+# File: update.sh
+_base=$(basename "$0")
+_dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P || exit 126)
+export _base _dir
+
+cd "${_dir}" || exit 126
+nix flake update
