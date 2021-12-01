306c32531ccdcc010dbf37e2340125fc915db67b
Mitchell Tishmack <git@mitchty.net>
2021-12-01 14:07:08 -0600

    Change the nixpkgs/unstable urls to be explicit

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "initial flake configuration setup";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-21.11";
-    unstable.url = "nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    unstable.url = "github:NixOS/nixpkgs/master";
     # Follow same nixpkgs as the system for home-manager
     home-manager.url = "github:nix-community/home-manager/release-21.05";
     # inputs.nixpkgs.follows = "nixpkgs";
