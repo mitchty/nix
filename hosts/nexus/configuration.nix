36e003fa60e704890adec51ba402c42e653b5e1a
Mitchell Tishmack <git@mitchty.net>
2021-12-13 19:19:15 -0600

    Migrate pipewire config to a file in prep for modules later

--- hosts/nexus/configuration.nix
+++ hosts/nexus/configuration.nix
@@ -5,6 +5,7 @@
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./pipewire.nix
     ];
 
   boot.loader.systemd-boot.enable = true;
@@ -207,107 +208,6 @@
   # Use the latest packaged kernel rev with zfs support
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 
-  # TODO: lets try pipewire, this needs review with OBS...
-  security.rtkit.enable = true;
-  hardware.pulseaudio.enable = false;
-  programs.dconf.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
-    # alsa.enable = true;
-    # alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-    socketActivation = true;
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    # media-session.enable = true;
-
-    config.pipewire-pulse = {
-      "context.properties" = {
-        "log.level" = 2;
-      };
-      "context.modules" = [
-        {
-          name = "libpipewire-module-rtkit";
-          args = {
-            "nice.level" = -15;
-            "rt.prio" = 88;
-            "rt.time.soft" = 200000;
-            "rt.time.hard" = 200000;
-          };
-          flags = [ "ifexists" "nofail" ];
-        }
-        { name = "libpipewire-module-protocol-native"; }
-        { name = "libpipewire-module-client-node"; }
-        { name = "libpipewire-module-adapter"; }
-        { name = "libpipewire-module-metadata"; }
-        {
-          name = "libpipewire-module-protocol-pulse";
-          args = {
-            "pulse.min.req" = "256/48000";
-            "pulse.default.req" = "256/48000";
-            "pulse.max.req" = "256/48000";
-            "pulse.min.quantum" = "256/48000";
-            "pulse.max.quantum" = "1024/48000";
-            "server.address" = [ "unix:native" ];
-          };
-        }
-      ];
-      "stream.properties" = {
-        "node.latency" = "256/48000";
-        "resample.quality" = 1;
-      };
-    };
-
-    config.pipewire = {
-      "context.properties" = {
-        "link.max-buffers" = 32;
-        "log.level" = 2;
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 256;
-        "default.clock.min-quantum" = 256;
-        "default.clock.max-quantum" = 1024;
-        "core.daemon" = true;
-        "core.name" = "pipewire-0";
-        "mem.allow-mlock" = true;
-      };
-      "context.modules" = [
-        {
-          name = "libpipewire-module-rtkit";
-          args = {
-            "nice.level" = -15;
-            "rt.prio" = 88;
-            "rt.time.soft" = 200000;
-            "rt.time.hard" = 200000;
-          };
-          flags = [ "ifexists" "nofail" ];
-        }
-        { name = "libpipewire-module-protocol-native"; }
-        { name = "libpipewire-module-profiler"; }
-        { name = "libpipewire-module-metadata"; }
-        { name = "libpipewire-module-spa-device-factory"; }
-        { name = "libpipewire-module-spa-node-factory"; }
-        { name = "libpipewire-module-client-node"; }
-        { name = "libpipewire-module-client-device"; }
-        {
-          name = "libpipewire-module-portal";
-          flags = [ "ifexists" "nofail" ];
-        }
-        {
-          name = "libpipewire-module-access";
-          args = { };
-        }
-        { name = "libpipewire-module-adapter"; }
-        { name = "libpipewire-module-link-factory"; }
-        { name = "libpipewire-module-session-manager"; }
-      ];
-    };
-  };
 
   # TODO: Lets re-enable ipv6 soon
   boot.kernelParams = [ "ipv6.disable=1" ];
