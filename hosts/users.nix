654896f84ae72c6223c48dc08a44cee1a29be76a
Mitchell Tishmack <git@mitchty.net>
2022-01-29 13:14:38 -0600

    effing syncthing

--- hosts/users.nix
+++ hosts/users.nix
@@ -1,35 +1,49 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let
   pubKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCl1r2eksJXO02QkuGbjVly38MhG9MpDfvQRPABWJLGfFIBQFNkCvvJffV1UEUpcRNNaAmle1DFS1CtvATZSr/UpTgzsAYu9X+gd0/5OB/WlWHJaC/j0H2LahtiUPKZ2d4/cLkKPQqP6HZdmOXrsHZR1I9bxjhqyNWhwxNLMCK/8995hKNWOYamMagJloHUTRLFQaor/WoFDqjfW8EKo09OxKnXtFFcj6CmXwsu1RWfFY/P/wsADL+8B2/P4CmqqwuLxQknbA0WZ2zWSj13tf24H7BORAkMAeK5249GuLd5SlnnvmHJLiF1OCIkSOZJMcyrNCCvBRavGLcPoKQbtHw7";
 in
 {
+  config.sops.secrets."users/root".neededForUsers = true;
+  config.sops.secrets."users/mitch".neededForUsers = true;
+
   # TODO: can home-manager sort this out now?
   # My normal user account
-  users.users.mitch = {
-    isNormalUser = true;
-    description = "Mitchell James Tishmack";
-    home = "/home/mitch";
-    createHome = true;
-    shell = pkgs.zsh;
-    initialPassword = "changeme";
-    extraGroups = [
-      "audio"
-      "docker"
-      "jackaudio"
-      "libvirtd"
-      "networkmanager"
-      "video"
-      "wheel"
-    ];
+  config.users = {
+    users = {
+      root = {
+        passwordFile = config.sops.secrets."users/root".path;
+      };
+      mitch = {
+        isNormalUser = true;
+        description = "Mitchell James Tishmack";
+        home = "/home/mitch";
+        createHome = true;
+        shell = pkgs.zsh;
+        passwordFile = config.sops.secrets."users/mitch".path;
+        extraGroups = [
+          "audio"
+          "docker"
+          "jackaudio"
+          "libvirtd"
+          "networkmanager"
+          "video"
+          "wheel"
+        ];
+      };
+    };
+
+    # Only users I define
+    mutableUsers = false;
+
+    # Setup the above public key as an authorized key.
+    extraUsers = {
+      root.openssh.authorizedKeys.keys = [ pubKey ];
+      mitch.openssh.authorizedKeys.keys = [ pubKey ];
+    };
   };
 
-  # TODO: figure this out
-  # Only what is defined, no manual crap
-  users.mutableUsers = false;
-
-  # Ssh key to allow for root/me
-  users.extraUsers = {
-    root.openssh.authorizedKeys.keys = [ pubKey ];
-    mitch.openssh.authorizedKeys.keys = [ pubKey ];
-  };
+  # allow passwordless sudo for wheel
+  config.security.sudo.extraConfig = ''
+    %wheel ALL=(root) NOPASSWD:ALL
+  '';
 }
