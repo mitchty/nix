5dc42bda99b91d9619490f86eca470dc869af8e6
Mitchell Tishmack <git@mitchty.net>
2022-02-04 10:26:09 -0600

    Default kernel setup/sysctl updates

--- hosts/kernel.nix
+++ hosts/kernel.nix
@@ -1,4 +1,17 @@
 { config, pkgs, ... }: {
-  # Use latest kernel rev (don't use on zfs...)
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    # Use latest kernel rev (don't use on zfs...)
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    # Always clean out /tmp
+    cleanTmpDir = true;
+
+    # always want sysrq keys in case I need to middle finger things to reboot
+    kernel.sysctl = {
+      "kernel.sysrq" = 1;
+    };
+
+    # For iotop
+    kernelParams = [ "delayacct" ];
+  };
 }
