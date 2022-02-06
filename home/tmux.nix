9ca42c47c429a1d75ffc1556baca7cd0d1335afc
Mitchell Tishmack <git@mitchty.net>
2022-02-05 19:44:20 -0600

    Add a tmuxp config for the mb system
    Will eventually expand this to be a "default" for any system. For now just puke
    the file everywhere.

--- home/tmux.nix
+++ home/tmux.nix
@@ -10,4 +10,7 @@
   # Setup some tmuxp configs
   home.file.".config/tmuxp/etc.yml".source = ./etc.yml;
   home.file.".config/tmuxp/nix.yml".source = ./nix.yml;
+
+  # TODO: Host specific configs, deal with conditionalizing later
+  home.file.".config/tmuxp/mb.yml".source = ./mb.yml;
 }
