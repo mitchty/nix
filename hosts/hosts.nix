1c59127f73225a093df1b058f5529087e38d04ef
Mitchell Tishmack <git@mitchty.net>
2022-03-05 19:39:48 -0600

    Add nexus to manual host setup

--- hosts/hosts.nix
+++ hosts/hosts.nix
@@ -2,7 +2,7 @@
   # Until I get the nix firewall setup
   networking.extraHosts = ''
     10.10.10.190 dfs1.home.arpa dfs1
-    10.10.10.200 slaptop.home.arpa slaptop
+    10.10.10.200 nexus.home.arpa nexus
     10.10.10.249 mb.home.arpa mb
   '';
 }
