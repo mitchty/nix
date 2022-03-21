9c442703cd8aee2d06ef2e7ff4d2ad4244798fa7
Mitchell Tishmack <git@mitchty.net>
2022-03-20 23:28:15 -0500

    hosts/kernel.nix: Starting to go back through defconfig options
    These are the easy gimmies to reduce compile times. Have to parse through the
    rest of defconfig now that zfs supports 5.16 again.

--- hosts/kernel.nix
+++ hosts/kernel.nix
@@ -5,7 +5,7 @@
     crashDump.enable = true;
 
     kernelPatches = pkgs.lib.singleton {
-      name = "Custom extra kernel config + maybe patches at some point";
+      name = "I like to customize my kernel config and patch it at times if a CVE comes out.";
       # Patch finally upstream
       patch = null;
       # patch = [
@@ -14,44 +14,37 @@
       # TODO: Validate all these CONFIG_ options make sense
       # The default nixpkgs kernel has these as modules, but whatever just
       # compile them in statically
-      # extraConfig = ''
+      extraConfig = ''
+        CAN n
+        FIREWIRE n
+        FPGA n
+        INFINIBAND n
+        INPUT_JOYSTICK n
+        INPUT_TOUCHSCREEN n
+        IWLWIFI n
+        GAMEPORT n
+        GREYBUS n
+        NET_VENDOR_NVIDIA n
+        NFC n
+        CXL_BUS n
+        STAGING y
+        STAGING_MEDIA y
+        ZSWAP_COMPRESSOR_DEFAULT_LZO n
+        ZSWAP_COMPRESSOR_DEFAULT_ZSTD y
+        ZSWAP_COMPRESSOR_DEFAULT zstd
+      '';
       #    DM_CACHE m
       #    DM_INTEGRITY m
       #    DM_VERITY m
       #    DM_THIN_PROVISIONING m
-      #    AX25 n
       #    CRYPTO_AEGIS128_SIMD n
-      #    CAN n
       #    BT n
       #    FB_NVIDIA n
       #    FB_NVIDIA_BACKLIGHT n
       #    FB_NVIDIA_I2C n
-      #    FIREWIRE n
-      #    FPGA n
-      #    INFINIBAND n
-      #    INPUT_JOYSTICK n
-      #    INPUT_TOUCHSCREEN n
-      #    IWLWIFI n
-      #    GAMEPORT n
-      #    GREYBUS n
-      #    NET_VENDOR_NVIDIA n
-      #    NFC n
       #    RTC_DRV_RK808 y
-      #    PCCARD n
-      #    PCMCIA n
-      #    PCMCIA_LOAD_CIS n
-      #    CARDBUS n
-      #    CXL_BUS n
-      #    CXL_PMEM n
-      #    CXL_ACPI n
       #    SND n
       #    SOUND n
-      #    STAGING y
-      #    STAGING_MEDIA y
-      #    ZSWAP_COMPRESSOR_DEFAULT_LZO n
-      #    ZSWAP_COMPRESSOR_DEFAULT_ZSTD y
-      #    ZSWAP_COMPRESSOR_DEFAULT zstd
-      #  '';
     };
 
     # Always clean out /tmp
