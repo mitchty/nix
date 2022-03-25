2fedd285207432cb2884e2dc8b2e6943708f4431
Mitchell Tishmack <git@mitchty.net>
2022-03-25 12:44:24 -0500

    tmuxp: refactor a bit, add a workmb profile

--- home/tmux.nix
+++ home/tmux.nix
@@ -8,9 +8,10 @@
   };
 
   # Setup some tmuxp configs
-  home.file.".config/tmuxp/etc.yml".source = ./etc.yml;
-  home.file.".config/tmuxp/nix.yml".source = ./nix.yml;
+  home.file.".config/tmuxp/etc.yml".source = ../static/tmuxp/etc.yml;
+  home.file.".config/tmuxp/nix.yml".source = ../static/tmuxp/nix.yml;
 
-  # TODO: Host specific configs, deal with conditionalizing later
-  home.file.".config/tmuxp/mb.yml".source = ./mb.yml;
+  # TODO: Host specific configs, deal with conditionalizing later in a module
+  home.file.".config/tmuxp/mb.yml".source = ../static/tmuxp/mb.yml;
+  home.file.".config/tmuxp/workmb.yml".source = ../static/tmuxp/workmb.yml;
 }
