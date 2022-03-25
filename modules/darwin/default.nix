74b0395cb050b6379df4c112de82c3093312467b
Mitchell Tishmack <git@mitchty.net>
2022-03-25 12:46:48 -0500

    darwin: Move more macos config into home manager

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -23,17 +23,39 @@
         remapCapsLockToControl = true;
       };
       defaults = {
-        trackpad.TrackpadRightClick = true;
+        NSGlobalDomain = {
+          "com.apple.mouse.tapBehavior" = 1;
+          "com.apple.sound.beep.feedback" = 0;
+          "com.apple.sound.beep.volume" = "0.0";
+          AppleKeyboardUIMode = 3;
+          ApplePressAndHoldEnabled = false;
+          AppleShowAllExtensions = true;
+          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticDashSubstitutionEnabled = false;
+          NSAutomaticPeriodSubstitutionEnabled = false;
+          NSAutomaticQuoteSubstitutionEnabled = false;
+          NSAutomaticSpellingCorrectionEnabled = false;
+        };
+        trackpad = {
+          Clicking = true;
+          Dragging = true;
+          TrackpadRightClick = true;
+        };
         dock = {
           autohide = true;
           autohide-delay = "0.0";
           mineffect = "suck";
           orientation = "bottom";
+          showhidden = true;
+          show-recents = false;
         };
         finder = {
+          AppleShowAllFiles = true;
+          ShowPathbar = true;
+          QuitMenuItem = true;
           AppleShowAllExtensions = true;
-          _FXShowPosixPathInTitle = true;
           FXEnableExtensionChangeWarning = false;
+          _FXShowPosixPathInTitle = true;
         };
       };
     };
