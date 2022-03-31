683e5420df8eb2d1712960fc0645afa1e11e05a2
Mitchell Tishmack <git@mitchty.net>
2022-03-30 19:53:17 -0500

    hm: Update packages and use unstable as pkgs source
    For most of these, e.g. git, its more useful to have the "latest...er" version.
    And with flake.lock I can easily revert if needed.

--- home/default.nix
+++ home/default.nix
@@ -20,7 +20,7 @@
   # TODO: should have a grouping or module setup where I can turn stuff on/off
   # aka have something to control: is this a box for streaming? if so add
   # obs-studio etc.. something akin to roles in ansible.
-  home.packages = with pkgs; [
+  home.packages = with inputs.unstable.legacyPackages.${pkgs.system}; [
     avahi
     bind
     coreutils
@@ -36,7 +36,7 @@
     gnumake
     htop
     inputs.mitchty.packages.${pkgs.system}.hwatch # TODO: Keep or no?
-    inputs.unstable.legacyPackages.${pkgs.system}.jless
+    jless
     less
     mercurial
     minio-client
@@ -44,45 +44,44 @@
     nix-prefetch-git
     nixpkgs-fmt
     openssl
+    p7zip
+    pbzip2
+    pigz
     podman
     pv
+    rage
     rclone
     restic
     ripgrep
-    sops
     s3cmd
     silver-searcher
     syncthing
     tldr
-    tmux
     tmuxp
     transcrypt
     unzip
     vim
     wget
     xz
+    yt-dlp
     # Non gui linux stuff
   ] ++ lib.optionals stdenv.isLinux [
-    tcpdump
-
     # Testing packages that I may/not upstream any changes to nixpkgs
     inputs.mitchty.packages.${pkgs.system}.seaweedfs
+    traitor
 
-    inputs.unstable.legacyPackages.${pkgs.system}.traitor
-
-    # Normal nixpkgs stuff
-    podman-compose
-
+    docker-compose
+    firefox
     lshw
+    podman-compose
+    tcpdump
+    xorg.xauth
     # Gui linux stuff
     # TODO: how do I get at the nixos config setup...?
     # ] ++ lib.optional (stdenv.isLinux && config.services.role.gui.enable) [
-    docker-compose
-    firefox
-    xorg.xauth
-    # macos only stuff
-  ] ++ lib.optionals stdenv.isDarwin [
-    yt-dlp
+
+    # macos only stuff (until I find something only macos empty...)
+    # ] ++ lib.optionals stdenv.isDarwin [
   ];
 
   # TODO: Finish porting emacs config over future me fix
