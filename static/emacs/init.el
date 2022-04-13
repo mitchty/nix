a2a66d1eb475041f75b5751e5426638289402657
Mitchell Tishmack <git@mitchty.net>
2022-04-13 09:39:37 -0500

    static/emacs: Change where use-package is coming from

--- static/emacs/init.el
+++ static/emacs/init.el
@@ -1,6 +1,7 @@
 ;; Just enough to load the "real" init file
 (require 'org)
 (require 'use-package)
+(setq use-package-verbose t)
 (defvar my/init-el (concat user-emacs-directory "readme.el"))
 
 (find-file (concat user-emacs-directory "readme.org"))
