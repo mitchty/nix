b743c9d6c3feeddb5e499cccb0639cf82b5777ce
Mitchell Tishmack <git@mitchty.net>
2022-05-06 07:46:22 -0500

    misc: Add my ancient who is on script
    Nostalgia mostly, not sure its useful anymore...

--- modules/darwin/mitchty.nix
+++ modules/darwin/mitchty.nix
@@ -17,6 +17,10 @@ let
   gohome = (pkgs.writeScriptBin "gohome" (builtins.readFile ../../static/src/gohome)).overrideAttrs (old: {
     buildCommand = "${old.buildCommand}\n patchShebangs $out";
   });
+
+  whoson = (pkgs.writeScriptBin "whoson" (builtins.readFile ../../static/src/whoson)).overrideAttrs (old: {
+    buildCommand = "${old.buildCommand}\n patchShebangs $out";
+  });
 in
 {
   options = {
@@ -36,6 +40,7 @@ in
       close
       gohome
       notify
+      whoson
     ];
   };
 }
