ff13323638ace5f2786a044c3d5738d2e3731a44
Mitchell Tishmack <git@mitchty.net>
2022-05-08 10:19:34 -0500

    chore: Migrate more profile to lib.sh

--- static/src/lib.sh
+++ static/src/lib.sh
@@ -13,6 +13,272 @@ randint() {
   awk "BEGIN{\"date +%N\"|getline rseed;srand(rseed);close(\"date +%N\");printf \"%i\n\", (rand()*${1-10})}"
 }
 
+# Because stuff sometimes fails, retry Until Success, this is just a silly
+# wrapper around until/sleep but I do it so often...
+us() {
+  until "$@"; do
+    sleep "$(randint)"
+  done
+}
+
+# Silly functions to make certain filtering easier/be lazy.
+nocomments() {
+  grep -Ev '^[#].*'
+}
+
+squishws() {
+  grep -Ev '^[[:space:]]*$'
+}
+
+hidepass() {
+  grep -Evi 'password'
+}
+
+# Silly helper functions to run stuff on specific platforms only
+onlinux() {
+  if [ "Linux" = "$(uname -s)" ]; then
+    "$@"
+  fi
+}
+
+onmac() {
+  if [ "Darwin" = "$(uname -s)" ]; then
+    "$@"
+  fi
+}
+
+# Old env related stuff, not sure if useful anymore but good examples of....
+# things written in anger I guess.
+
+# cat out a : separated env variable
+# variable is the parameter
+cat_env() {
+  set | grep '^'"$1"'=' > /dev/null 2>&1 && eval "echo \$$1" | tr ':' '
+' | awk '!/^\s+$/' | awk '!/^$/'
+}
+
+# Convert a line delimited input to : delimited
+to_env() {
+  awk '!a[$0]++' < /dev/stdin | tr -s '
+' ':' | sed -e 's/\:$//' | awk 'NF > 0'
+}
+
+# Unshift a new value onto the env var
+# first arg is ENV second is value to unshift
+# basically prepend value to the ENV variable
+unshift_env() {
+  new=$(eval "echo $2; echo \$$1" | to_env)
+  eval "$1=${new}; export $1"
+}
+
+# Opposite of above, but echos what was shifted off
+shift_env() {
+  first=$(cat_env "$1" | head -n 1)
+  rest=$(cat_env "$1" | awk '{if (NR!=1) {print}}' | to_env)
+  eval "$1=$rest; export $1"
+  echo "${first}"
+}
+
+# push $2 to $1 on the variable
+push_env() {
+  have=$(cat_env "$1")
+  new=$(printf "%s
+%s" "$have" "$2" | to_env)
+  eval "$1=$new; export $1"
+}
+
+# Remove a line matched in $HOME/.ssh/known_hosts for when there are legit
+# host key changes.
+nukehost() {
+  for x in echo "$@"; do
+    sed -i -e "/$x/d" ~/.ssh/known_hosts
+  done
+}
+
+# Git wrapper fn used like pushd/popd in that on success, you're in a dir/checkout
+
+try_git() {
+  # assume https if input doesn't contain a protocol string :// otherwise treat
+  # it as golden and let git complain or not
+  uproto=$(printf "%s" "${1}" | grep '://' > /dev/null 2>&1 && printf "%s" "${1}" | sed -e 's|[:]\/\/.*||g')
+  proto=${uproto:-https}
+  defbranches="${2:-master main}"
+  # For wrapper functions, allow destination to be an env var iff unset and
+  # nonnull and $3 is present use $3 or as a fallback use $HOME/src
+  destination=${DEST:=${3:=$HOME/src/pub}}
+
+  git_dir=$(printf "%s" "${1}" | sed -e 's|.*[:]\/\/||g')
+  rrepo="${proto}://${git_dir}"
+
+  # strip user@, :NNN, and .git from input uri's
+  repo="${destination}/"$(printf "%s" "${git_dir}" |
+    sed -e 's/\.git$//g' |
+    sed -e 's|.*\@||g' |
+    sed -e 's|\:[[:digit:]]\{1,\}\/|/|g' |
+    tr -d '~')
+
+  # Loop through the def(ault)branches looking for a git repo to clone
+  ocwd=$(pwd)
+  # For this function who cares about these warnings
+  # shellcheck disable=SC2116 disable=SC2086
+  for branch in $(echo ${defbranches}); do
+    # If we git clone(d) and/or changed to that dir already, don't bother
+    # looping, just break
+    [ "${ocwd}" != "$(pwd)" ] && break
+
+    if [ ! -d "${repo}" ]; then
+      if git ls-remote "${rrepo}" > /dev/null 2>&1; then
+        install -dm755 "${repo}"
+        printf "git clone %s %s\n" "${rrepo}" "${repo}" >&2
+        git clone --recursive "${rrepo}" "${repo}" || rmdir "${repo}"
+      else
+        printf "%s doesn't look to be a git repository\n" "${rrepo}" >&2
+        return 1
+      fi
+    fi
+
+    # Treat branch(es) "master" and "main" as special branches we don't treat
+    # as workdir clone directories.
+    if [ "${branch}" != "master" ] && [ "${branch}" != "main" ]; then
+      wtdir="${repo}@${branch}"
+      if [ ! -d "${wtdir}" ]; then
+        if git branch -r --list 'origin/*' | grep -E "^\s+origin/${branch}$" > /dev/null 2>&1; then
+          git worktree add "${repo}@${branch}" "${branch}"
+        else
+          printf "%s at branch %s not present in repo %s\n" "${wtdir}" "${branch}" "${rrepo}" >&2
+          break
+        fi
+      fi
+
+      # shellcheck disable=SC2164
+      cd "${wtdir}" && break
+    fi
+
+    if [ -d "${repo}" ]; then
+      # shellcheck disable=SC2164
+      cd "${repo}"
+    fi
+  done
+
+  # If we didn't chdir() to the ${repo} let user know.
+  if [ "${ocwd}" = "$(pwd)" ] && [ "$(pwd)" != "${repo}" ]; then
+    printf "error: couldn't git clone %s to %s\n" "${rrepo}" "${repo}" >&2
+    return 1
+  fi
+
+  # Otherwise act like cd and print out the dir we're in to stderr
+  printf "%s\n" "$(pwd)" | sed -e "s|$HOME|~|" >&2
+}
+
+mt() {
+  try_git "ssh://gitea@git.mitchty.net:2222/${1}" "${2:-master}"
+}
+
+gh() {
+  try_git "https://github.com/${1}" "${2:-master main}"
+}
+
+bb() {
+  try_git "https://bitbucket.org/${1}" "${2:-master main}"
+}
+
+# TODO Shamelessly copy/pasting from try_git, future me figure out a way to bridge the two
+try_hg() {
+  # assume https if input doesn't contain a protocol string :// otherwise treat
+  # it as golden and let hg complain or not
+  uproto=$(printf "%s" "${1}" | grep '://' > /dev/null 2>&1 && printf "%s" "${1}" | sed -e 's|[:]\/\/.*||g')
+  proto=${uproto:-https}
+  defbranches="${2:-master main}"
+  # For wrapper functions, allow destination to be an env var iff unset and
+  # nonnull and $3 is present use $3 or as a fallback use $HOME/src
+  destination=${DEST:=${3:=$HOME/src/pub}}
+
+  hg_dir=$(printf "%s" "${1}" | sed -e 's|.*[:]\/\/||g')
+  rrepo="${proto}://${hg_dir}"
+
+  # strip user@, :NNN from input uri's
+  repo="${destination}/"$(printf "%s" "${hg_dir}" |
+    sed -e 's|.*\@||g' |
+    sed -e 's|\:[[:digit:]]\{1,\}\/|/|g' |
+    tr -d '~')
+
+  # Loop through the def(ault)branches looking for a git repo to clone
+  ocwd=$(pwd)
+  # For this function who cares about these warnings
+  # shellcheck disable=SC2116 disable=SC2086
+  for branch in $(echo ${defbranches}); do
+    # If we hg clone(d) and/or changed to that dir already, don't bother
+    # looping, just break
+    [ "${ocwd}" != "$(pwd)" ] && break
+
+    if [ ! -d "${repo}" ]; then
+      if hg identify "${rrepo}" > /dev/null 2>&1; then
+        install -dm755 "${repo}"
+        printf "hg clone %s %s\n" "${rrepo}" "${repo}" >&2
+        hg clone "${rrepo}" "${repo}" || rmdir "${repo}"
+      else
+        printf "%s doesn't look to be an hg repository\n" "${rrepo}" >&2
+        return 1
+      fi
+    fi
+
+    # TODO: does hg support workdirs like git? for now whatever
+    # Treat branch(es) "master" and "main" as special branches we don't treat
+    # as workdir clone directories.
+    # if [ "${branch}" != "master" ] && [ "${branch}" != "main" ]; then
+    #   wtdir="${repo}@${branch}"
+    #   if [ ! -d "${wtdir}" ]; then
+    #     if git branch -r --list 'origin/*' | grep -E "^\s+origin/${branch}$" > /dev/null 2>&1; then
+    #       git worktree add "${repo}@${branch}" "${branch}"
+    #     else
+    #       printf "%s at branch %s not present in repo %s\n" "${wtdir}" "${branch}" "${rrepo}"  >&2
+    #       return 1
+    #     fi
+    #   fi
+
+    #   # shellcheck disable=SC2164
+    #   cd "${wtdir}"
+    # fi
+
+    if [ -d "${repo}" ]; then
+      # shellcheck disable=SC2164
+      cd "${repo}"
+    fi
+  done
+
+  # If we didn't chdir() to the ${repo} let user know.
+  if [ "${ocwd}" = "$(pwd)" ] && [ "$(pwd)" != "${repo}" ]; then
+    printf "error: couldn't hg clone %s to %s\n" "${rrepo}" "${repo}" >&2
+    return 1
+  fi
+
+  # Otherwise act like cd and print out the dir we're in to stderr
+  printf "%s\n" "$(pwd)" | sed -e "s|$HOME|~|" >&2
+}
+
+# Wrapper functions to call whatever into a different prefix
+# aka gh foo/bar -> ~/src/github.com/pub/foo/bar
+# wrk gh foo/bar -> ~/src/github.com/wrk/foo/bar
+# prv gh foo/bar -> ~/src/github.com/prv/foo/bar
+#
+# Note: this then means can't call them with the final arg for say gh/bb
+wrk() {
+  DEST="${HOME}/src/${0}" "$@"
+}
+
+prv() {
+  DEST="${HOME}/src/${0}" "$@"
+}
+
+# To be lazy
+orgprv() {
+  prv mt org-mode/private
+}
+
+orgpub() {
+  mt org-mode/public
+}
+
 # Retry N times some command/thing, requires the limit to be first arg.
 #
 # On success running thing within the limit set, returns 0
