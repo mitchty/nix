0f16407d1234585f5a7eb96445b239d184c95ff5
Mitchell Tishmack <git@mitchty.net>
2022-06-03 17:44:51 -0500

    hm: Fix/update/remove a silly warning message if using nix-shell

--- modules/home-manager/registry.nix
+++ modules/home-manager/registry.nix
@@ -23,17 +23,14 @@ let
   };
 in
 {
-  home.file =
-    folder-inputs
-    // {
-      ".config/nix/registry.json".text = registry;
-    };
-  # This don't work with tmux! Pour quoi?
-  # home.sessionVariables.NIX_PATH = "nixpkgs=${config.home.homeDirectory}/.nix-inputs/self$\{NIX_PATH:+:$NIX_PATH}";
+  home.file = folder-inputs // {
+    ".config/nix/registry.json".text = registry;
+  };
   home.activation.useFlakeChannels = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     $DRY_RUN_CMD rm -rf $VERBOSE_ARG ~/.nix-defexpr
-    $DRY_RUN_CMD ln -s $VERBOSE_ARG /dev/null $HOME/.nix-defexpr
     $DRY_RUN_CMD rm -rf $VERBOSE_ARG ~/.nix-channels
+    $DRY_RUN_CMD install -dm755 $VERBOSE_ARG $HOME/.nix-defexpr
+    $DRY_RUN_CMD ln -s $VERBOSE_ARG /dev/null $HOME/.nix-defexpr/channels
     $DRY_RUN_CMD ln -s $VERBOSE_ARG /dev/null $HOME/.nix-channels
     $DRY_RUN_CMD ln -sf $VERBOSE_ARG /dev/null $HOME/.config/nixpkgs
   '';
