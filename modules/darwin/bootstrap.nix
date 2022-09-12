67af0e60e92d51e3accf42503fdaf6c469fe527f
Mitchell Tishmack <git@mitchty.net>
2022-09-12 12:46:42 -0500

    darwin: Update to use new nix-darwin setup

--- modules/darwin/bootstrap.nix
+++ modules/darwin/bootstrap.nix
@@ -1,29 +1,26 @@
-{ pkgs, ... }:
-let
-  mkCache = url: key: { inherit url key; };
-  caches =
-    let
-      nixos = mkCache "https://cache.nixos.org" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
-      nix-community = mkCache "https://nix-community.cachix.org" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
-      nix-cache = mkCache "http://nixcache.home.arpa" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
-    in
-    [ nix-cache nixos nix-community ];
+{ pkgs, ... }: {
+  nix = {
+    settings = {
+      trusted-users = [
+        "@admin"
+      ];
+      substituters = [
+        "http://nixcache.home.arpa"
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+    configureBuildUsers = true;
+    extraOptions = ''
+      auto-optimise-store = true
+      experimental-features = nix-command flakes
+    '';
+  };
 
-  customCaches = map (x: x.url) caches;
-in
-{
-  nix.binaryCaches = customCaches;
-  nix.binaryCachePublicKeys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-  ];
-  nix.trustedUsers = [
-    "@admin"
-  ];
-  users.nix.configureBuildUsers = true;
-  nix.extraOptions = ''
-    auto-optimise-store = true
-    experimental-features = nix-command flakes
-  '';
   services.nix-daemon.enable = true;
   # TODO: look into this a bit more
   # environment.shells = with pkgs; [
