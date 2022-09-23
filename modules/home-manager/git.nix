0d863b9d6dbffc5a2d88468accb8b3a0f2af7506
Mitchell Tishmack <git@mitchty.net>
2022-09-23 09:08:48 -0500

    work: Add in ~/src/wrk for gitconfig

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,5 +3,19 @@
   # operations on say github. Probably best to put it "somewhere else".
   home.file.".netrc".source = config.lib.file.mkOutOfStoreSymlink age.secrets."git/netrc".path;
   home.file.".gitignore".source = ../../static/git/ignore;
-  home.file.".gitconfig".source = ../../static/git/config;
+
+  # TODO: This is just a quick hack to get git config setup for now. Future me
+  # make sure there isn't a programs.git or make this whole thing a module...
+  home.file.".gitconfig-work".text = ''
+
+    [user]
+      name = Mitchell Tishmack
+      email = mitchell.james.tishmack@hpe.com
+  '';
+
+  home.file.".gitconfig".text = (builtins.readFile ../../static/git/config) + ''
+    # it worked
+        [includeIf "gitdir:~/src/wrk/"]
+           path = .gitconfig-work
+  '';
 }
