2720f22e0daa0e8b0d16ed810b5ac7e094c279eb
Mitchell Tishmack <git@mitchty.net>
2022-09-25 10:01:06 -0500

    hm: Move stglobalignore to a file and expand what it ignores a bit

--- modules/home-manager/syncthing.nix
+++ modules/home-manager/syncthing.nix
@@ -3,15 +3,6 @@
     ".stignore".text = ''
       #include .stglobalignore
     '';
-    ".stglobalignore".text = ''
-      .git/COMMIT_EDITMSG
-      (?d).DS_Store
-      packer_cache
-      result
-      target/release
-      target/debug
-      domain/attic
-      tmp_pack_*
-    '';
+    ".stglobalignore".text = (builtins.readFile ./stglobalignore);
   };
 }
