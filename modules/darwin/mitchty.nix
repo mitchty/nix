fc6249f6c167169185fe200f85f6c75c95fee31b
Mitchell Tishmack <git@mitchty.net>
2022-09-24 22:29:39 -0500

    chore: make obs a package as well too

--- modules/darwin/mitchty.nix
+++ modules/darwin/mitchty.nix
@@ -6,6 +6,7 @@ let
   cfg = config.services.mitchty;
   fake = lib.fakeSha256;
 
+  obs-studio = pkgs.callPackage ./pkgs/obs.nix { };
   stats = pkgs.callPackage ./pkgs/stats.nix { };
 
   # Scripts I wrote that are macos only.
@@ -47,29 +48,6 @@ let
       name = "${aname}.dmg";
       url = "http://get.videolan.org/${name}/${version}/macosx/${name}-${version}-intel64.dmg";
       sha256 = "sha256-zKJn8sUa5WjgLztNimrbqHsr3l8BGiuHxZXBAtifEdg=";
-    };
-  };
-
-  obs-studio = with pkgs; stdenv.mkDerivation rec {
-    name = "obs-studio";
-    uname = "obsproject";
-    aname = "OBS";
-    version = "28.0.1";
-
-    sourceRoot = ".";
-    phases = [ "installPhase" ];
-    installPhase = ''
-      install -dm755 "$out/Applications"
-      VOLUME=`/usr/bin/hdiutil attach $src | grep Volumes | awk '{print $3}'`
-      cp -rf $VOLUME/${aname}.app $out/Applications/${aname}.app
-      /usr/bin/hdiutil detach $VOLUME
-    '';
-
-    src = fetchurl {
-      name = "${aname}.dmg";
-      # As of obs 28.0.0 this disk image is now APFS and not HFS, so undmg no worky
-      url = "https://github.com/${uname}/${name}/releases/download/${version}/obs-studio-${version}-macos-x86_64.dmg";
-      sha256 = "sha256-JG4bF0rz9hUT5OAE6UijJRSQkbUWW3AFORVw9tM07j4=";
     };
   };
 
