e265324c13163700690fbdba92289e302e49cd70
Mitchell Tishmack <git@mitchty.net>
2022-10-30 14:28:53 -0500

    emacs: Test out using use-package for emac config too

--- static/emacs/init.el
+++ static/emacs/init.el
@@ -4,6 +4,12 @@
 (setq use-package-verbose t)
 (defvar my/init-el (concat user-emacs-directory "readme.el"))
 
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(when (not (window-system))
+  (menu-bar-mode -1))
+
 (find-file (concat user-emacs-directory "readme.org"))
 (org-babel-tangle)
 (load-file my/init-el)
