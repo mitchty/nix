66fa03df3ddabebab3d7e2aef0c1f4d17f3ff46d
Mitchell Tishmack <git@mitchty.net>
2022-11-24 12:01:40 -0600

    chore: convert tmuxp config/setup to be defined in nix

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,4 +1,145 @@
-{ ... }: {
+{ lib, ... }:
+let
+  # TODO: Learn how to unit test this stuff
+  base = {
+    start_directory = "~/";
+  };
+
+  # TODO: need to nix-ify the syncthing setup I have
+  syncthing = {
+    window_name = "syncthing";
+    panes = [ "pkill syncthing; syncthing" ];
+  };
+
+  initial = {
+    window_name = "sh";
+    panes = [ "pane" ];
+    focus = true;
+  };
+
+  btop = {
+    window_name = "btop";
+    panes = [ "btop" ];
+  };
+
+  nix = {
+    window_name = "gh/nix";
+    layout = "even-vertical";
+    panes = [ "gi mitchty/nix" ];
+  };
+
+  nixos = {
+    window_name = "gh/nixos";
+    layout = "even-vertical";
+    panes = [
+      {
+        shell_command = [
+          "ssh -t srv.home.arpa \"zsh --login -i -c 'gi mitchty/nixos && nci'\""
+        ];
+      }
+      {
+        shell_command = [
+          "gi mitchty/nixos"
+          "nci"
+        ];
+      }
+      {
+        focus = true;
+        shell_command = [
+          "gi mitchty/nixos"
+        ];
+      }
+    ];
+  };
+
+  rswip = {
+    window_name = "rswip";
+    layout = "even-vertical";
+    panes = [
+      {
+        shell_command = [
+          "ssh -t srv.home.arpa \"zsh --login -i -c 'cd ~/src/pub/wip/ria/grep-lite && nrsci'\""
+        ];
+      }
+      {
+        shell_command = [
+          "cd ~/src/pub/wip/ria/grep-lite && nrsci"
+        ];
+      }
+      {
+        focus = true;
+        shell_command = [
+          "~/src/pub/wip/ria/grep-lite"
+        ];
+      }
+    ];
+  };
+
+  yolo = {
+    window_name = "yolo";
+    panes = [ ". ~/src/pub/github.com/mitchty/nix/static/src/ssh.sh" ];
+  };
+
+  wip = {
+    window_name = "wip";
+    panes = [ "pane" ];
+  };
+
+  deploy = {
+    shell_command = [
+      "gi mitchty/nix"
+      {
+        cmd = "deploy";
+        enter = false;
+      }
+    ];
+  };
+
+  site-update = {
+    shell_command = [
+      "gi mitchty/nix"
+      {
+        cmd = "site-update";
+        enter = false;
+      }
+    ];
+  };
+
+  deployall = {
+    window_name = "deploy";
+    panes = [ site-update deploy ];
+  };
+
+  deploylocal = {
+    window_name = "deploy";
+    panes = [ site-update deploy ];
+  };
+
+  # nix = {
+  #   window_name = "nixdev";
+  #   panes =[
+  #     {
+  #       shell_command = [
+  #         "gi mitchty/nix"
+  #         {
+  #           cmd = "site-update";
+  #           enter = "false";
+  #         }
+  #       ];
+  #     }
+  #     {
+  #       shell_command = [
+  #         "gi mitchty/nix"
+  #         {
+  #           cmd = "deploy";
+  #           enter = "false";
+  #         }
+  #       ];
+  #     }
+  #   ];
+  # };
+in
+rec {
   programs.tmux = {
     enable = true;
     keyMode = "vi";
@@ -8,10 +149,47 @@
   };
 
   # Setup some tmuxp configs
-  home.file.".config/tmuxp/etc.yml".source = ../../static/tmuxp/etc.yml;
-  home.file.".config/tmuxp/nix.yml".source = ../../static/tmuxp/nix.yml;
+  home.file.".config/tmuxp/etc.yml".text = (lib.generators.toYAML { } {
+    start_directory = "~/";
+    session_name = "etc";
+    windows = [ initial syncthing btop ];
+  });
 
-  # TODO: Host specific configs, deal with conditionalizing later in a module
-  home.file.".config/tmuxp/mb.yml".source = ../../static/tmuxp/mb.yml;
-  home.file.".config/tmuxp/wmb.yml".source = ../../static/tmuxp/wmb.yml;
+  home.file.".config/tmuxp/wip.yml".text = (lib.generators.toYAML { } {
+    start_directory = "~/";
+    session_name = "wip";
+    windows = [ wip nix nixos rswip ];
+  });
+
+  home.file.".config/tmuxp/mb.yml".text = (lib.generators.toYAML { } {
+    start_directory = "~/";
+    session_name = "mb";
+    windows = [ initial syncthing deployall ];
+  });
+
+  # TODO: Get syncthing into a user systemd unit or something
+  home.file.".config/tmuxp/srv.yml".text = (lib.generators.toYAML { } {
+    start_directory = "~/";
+    session_name = "srv";
+    windows = [ initial syncthing btop ];
+  });
+
+  # TODO: Get syncthing into a user systemd unit or something
+  home.file.".config/tmuxp/nexus.yml".text = (lib.generators.toYAML { } {
+    start_directory = "~/";
+    session_name = "nexus";
+    windows = [ initial syncthing btop ];
+  });
+
+  home.file.".config/tmuxp/sys1.yml".text = (lib.generators.toYAML { } {
+    start_directory = "~/";
+    session_name = "sys1";
+    windows = [ initial btop ];
+  });
+
+  home.file.".config/tmuxp/wmb.yml".text = (lib.generators.toYAML { } {
+    start_directory = "~/";
+    session_name = "wmb";
+    windows = [ initial deploylocal yolo wip ];
+  });
 }
