d30028453458915361cdd5a2e39d787e0c2264f2
Mitchell Tishmack <git@mitchty.net>
2022-11-26 12:43:14 -0600

    emacs: simplify early-init.el to just load org directly

--- static/emacs/early-init.el
+++ static/emacs/early-init.el
@@ -1,17 +1,12 @@
 ;; Just enough to load the "real" init file
 (require 'org)
 (require 'use-package)
-(setq use-package-verbose t)
-(defvar my/init-el (concat user-emacs-directory "init.el"))
-
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+(setq use-package-verbose t
+      tool-bar-mode -1
+      scroll-bar-mode -1)
 (when (not (window-system))
   (menu-bar-mode -1))
 
 (setq-default debug-on-error t)
 
-(find-file (concat user-emacs-directory "init.org"))
-(org-babel-tangle)
-(load-file my/init-el)
-;;(byte-compile-file my/init-el)
+(org-babel-load-file (concat user-emacs-directory "init.org"))
