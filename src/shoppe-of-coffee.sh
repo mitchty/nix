773f24de40d9904ba339522cb2ba4b2d78d4595c
Mitchell Tishmack <git@mitchty.net>
2023-07-26 09:38:17 -0500

    wip: hack wrapper script to cover do stuff when at a shoppe of coffee

---
+++ src/shoppe-of-coffee.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+#-*-mode: Shell-script; coding: utf-8;-*-
+# SPDX-License-Identifier: BlueOak-1.0.0
+# Description: Just a dum af wrapper around vpn-hack.sh
+_base=$(basename "$0")
+_dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P || exit 126)
+export _base _dir
+set "${SETOPTS:--eu}"
+
+# Just a quick hack to let me specify multiple files to the hack script.
+for f in "$@"; do
+  if [ -e "${f}" ]; then
+    "${HOME}/src/pub/github.com/mitchty/nix/src/vpn-hack.sh" < "${f}"
+  fi
+done
