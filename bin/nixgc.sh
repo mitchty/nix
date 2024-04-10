c4f3ccea167aff0daf401bb1d459549f85f66e9d
Mitchell Tishmack <git@mitchty.net>
2024-04-09 21:37:14 -0500

    misc: move nixgc to a script

---
+++ bin/nixgc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#-*-mode: Shell-script; coding: utf-8;-*-
+# SPDX-License-Identifier: BlueOak-1.0.0
+# Description:
+_base=$(basename "$0")
+_dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P || exit 126)
+export _base _dir
+set "${SETOPTS:--eu}"
+
+nix-collect-garbage --delete-older-than 31d
+nix-store --gc
+nix-store --optimise
