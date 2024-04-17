4c00382d4c89c67c58c821028734c9d6dcd54712
Mitchell Tishmack <git@mitchty.net>
2024-04-17 07:46:38 -0500

    overlay

--- nix/overlays/hponcfg.nix
+++ nix/overlays/hponcfg.nix
@@ -1,6 +1,10 @@
 _self: super: {
-  hponcfg = super.pkgs.callPackage ../pkgs/hponcfg.nix
-    {
-      inherit (super) pkgs;
-    };
+  hponcfg = super.pkgs.callPackage ../pkgs/hponcfg.nix {
+    fetchurl = super.pkgs.fetchurl;
+    rpmextract = super.pkgs.rpmextract;
+    openssl = super.pkgs.openssl;
+    busybox = super.pkgs.busybox;
+    autoPatchelfHook = super.pkgs.autoPatchelfHook;
+    makeWrapper = super.pkgs.makeWrapper;
+  };
 }
