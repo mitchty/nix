ecc566770ae44dc316fcc33e59b0759d4e5d80ba
Mitchell Tishmack <git@mitchty.net>
2024-10-29 22:04:09 -0500

    secret updates

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -10,20 +10,20 @@ let
   srv = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINsX6e+fhe/CxoGIbZ4auuk83H3sUK5XQhia8OWFz4pt";
   nexus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCQJlqfzBYIjuWAIl72Q4o264vMEKWc4b+Tc30cqgtO";
   wm2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJNzRSDjB8WJHSEepNu2GTrZIgFWprv+wMnX6xbeoD0U";
-  dfs1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIIXtwtlXD59ni6Y/+jYr2opNqvG6sTTXKbVN4OBLTA";
+  rtx = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKBaFOFERMbg/d7DHrTBJ7pPKiJhwxFadQZlagalg51/";
 
   cl1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOxV4KEVMkikEM4L9QCvd8QcMwvDK3nryBL28L0BFffZ";
   cl2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO9/+zDNc2RTZNn25SN0z/iKBc6RrT+uleTUaJT+nPIh";
   cl3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFjWCCiZOEVe0MWZgpJSMQKrXdA26x8MuaTM7gI6qLYN";
 
-  allnixos = [ dfs1 srv nexus wm2 gw cl1 cl2 cl3 ];
+  allnixos = [ rtx srv wm2 gw cl1 cl2 cl3 nexus ];
 
   # To make the following a skosh simpler/easier
   homeusers = [ mitch ];
   homehosts = [ mb ] ++ allnixos;
 
   git = homehosts ++ homeusers;
-  restic = [ mb srv nexus ];
+  backup = [ mb srv wm2 ];
 
   # Some secrets should be usable everywhere
   allusers = homeusers;
@@ -34,14 +34,16 @@ let
 
   # Mostly for the canary secret for testing
   everything = allusers ++ allhosts;
+
+  # Cifs hosts
+  cifs = [ srv wm2 rtx nexus ];
+
+  # ytdl
+  ytdl = [ srv nexus ];
 in
 {
   # Just a canary file to know if things are working
   "canary.age".publicKeys = everything;
-
-  # Using an env file that the launchd script sources, hopefully systemd can use
-  # this directly as an env file
-  "restic/env.sh.age".publicKeys = restic ++ ageadmins;
 
   # For authenticated git push/pull mainly.
   "git/netrc.age".publicKeys = git ++ ageadmins;
@@ -53,4 +55,11 @@ in
 
   # Only the router needs the the hostapd stuff
   "wifi/passphrase.age".publicKeys = router ++ ageadmins;
+
+  # cifs mount user/pass files
+  "cifs/plex.age".publicKeys = cifs ++ ageadmins;
+  "cifs/mitch.age".publicKeys = cifs ++ ageadmins;
+
+  # Cookies for ytdl-sub
+  "net/cookies.txt.age".publicKeys = ytdl ++ ageadmins;
 }
