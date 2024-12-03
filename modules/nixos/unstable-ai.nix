b5a6b1a298c845a12f097f2181fed3147de15f63
Mitchell Tishmack <git@mitchty.net>
2024-12-03 11:28:53 -0600

    chore: not really using this but might resurrect it later

--- modules/nixos/unstable-ai.nix
+++ modules/nixos/unstable-ai.nix
@@ -3,14 +3,14 @@
   ... }: {
     imports = let
       inherit (specialArgs) system;
-      inherit (inputs.latest.legacypackages.${system}) open-webui ollama;
+      inherit (inputs.latest.legacyPackages.${system}) open-webui ollama;
     in [
       # Disable 24.05 ollama nixos service and use unstable ollama package
       {
-        disabledModules = [ "services/misc/ollama.nix" ];
-        nixpkgs.overlays = [ (self: super: { inherit ollama; }) ];
+        # disabledModules = [ "services/misc/ollama.nix" ];
+        # nixpkgs.overlays = [ (self: super: { inherit ollama; }) ];
       }
-      (inputs.latest + /nixos/modules/services/misc/ollama.nix)
-      (inputs.latest + /nixos/modules/services/misc/open-webui.nix)
+#      (inputs.latest + /nixos/modules/services/misc/ollama.nix)
+ #     (inputs.latest + /nixos/modules/services/misc/open-webui.nix)
     ];
   }
