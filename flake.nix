c4ef0281e14b329a7a2912cbc5d01640b19e6f8e
Mitchell Tishmack <git@mitchty.net>
2025-05-10 09:04:00 -0500

    chore: migrate flake overlay to its own file
    Keeps main flake.nix a skosh cleaner.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,15 @@
       ...
     }@inputs:
     flakelight ./. (
-      { lib, stdenv, ... }:
+      # TODO: add https://github.com/serokell/deploy-rs to the
+      # nixosConfigurations to validate that setup.
+      {
+        lib,
+        stdenv,
+        pkgs,
+        moduleArgs,
+        ...
+      }:
       {
         inherit inputs;
         # All here and not in ./nix cause I don't feel like doing it better,
@@ -24,40 +32,7 @@
           "aarch64-darwin"
         ];
 
-        withOverlays = [
-          (final: prev: {
-            # Exposes each input as pkgs.name in the normal package set
-            #
-            # Not quite an "overlay" but a way to abuse different package inputs
-            # or use all of em if I want in derivations here.
-            release = import inputs.nixpkgs-release {
-              inherit (prev) system;
-              config = {
-                allowUnfree = true;
-              };
-            };
-            # TODO: Should I even keep this here? Also nix-hardware needs to get
-            # in here at some point.
-            open-webui-cli = inputs.open-webui-cli.packages.${prev.system}.release;
-            inherit (inputs.nix-update.packages.${prev.system}) nix-update;
-            inherit (inputs.nixos-generators.packages.${prev.system}) nixos-generate;
-            inherit (inputs.home-manager.packages.${prev.system}) home-manager;
-            # workaround for macos
-            # https://github.com/NixOS/nixpkgs/issues/402079#issuecomment-2846520987
-            # for bash-language-server/yaml-language-server ultimately, mabye I
-            # skip it for a while till the fix gets into 24.11
-            nodejs = prev.nodejs_22;
-            nodejs-slim = prev.nodejs-slim_22;
-          })
-          inputs.emacs-overlay.overlay
-          inputs.deploy-rs.overlay
-          inputs.agenix.overlays.default
-          inputs.fenix.overlays.default
-          inputs.nur.overlays.default
-          inputs.self.overlays.overrides
-          inputs.self.overlays.emacs
-          inputs.self.overlays.yt-dlp
-        ];
+        withOverlays = import ./nix/flakeOverlays.nix moduleArgs;
 
         checks = {
           altshfmt = pkgs: pkgs.altshfmt;
@@ -83,8 +58,8 @@
           myFonts = pkgs: pkgs.paid-fonts;
           statix = pkgs: "${pkgs.statix}/bin/statix check";
           # }
-          # TODO: how this isn't working is beyond me for now wgaf I'm not using it yet future me problem.
-          # // lib.optionalAttrs stdenv.isLinux {
+          # # TODO: how this isn't working is beyond me for now wgaf I'm not using it yet future me problem.
+          # // lib.optionalAttrs lib.stdenv.hostPlatform.isLinux {
           #   # Make sure my overlay for this thing works but only on linux
           #   openwebui = pkgs: pkgs.open-webui;
         };
---
+++ nix/flakeOverlays.nix
@@ -0,0 +1,34 @@
+{ inputs, ... }:
+[
+  (final: prev: {
+    # Exposes each input as pkgs.name in the normal package set
+    #
+    # Not quite an "overlay" but a way to abuse different package inputs
+    # or use all of em if I want in derivations here.
+    release = import inputs.nixpkgs-release {
+      inherit (prev) system;
+      config.allowUnfree = true;
+      config.allowBroken = true; # darwin/aarch64 OVMF marked broken
+    };
+    # TODO: Should I even keep this here? Also nix-hardware needs to get
+    # in here at some point.
+    open-webui-cli = inputs.open-webui-cli.packages.${prev.system}.release;
+    inherit (inputs.nix-update.packages.${prev.system}) nix-update;
+    inherit (inputs.nixos-generators.packages.${prev.system}) nixos-generate;
+    inherit (inputs.home-manager.packages.${prev.system}) home-manager;
+    # workaround for macos
+    # https://github.com/NixOS/nixpkgs/issues/402079#issuecomment-2846520987
+    # for bash-language-server/yaml-language-server ultimately, mabye I
+    # skip it for a while till the fix gets into 24.11
+    nodejs = prev.nodejs_22;
+    nodejs-slim = prev.nodejs-slim_22;
+  })
+  inputs.emacs-overlay.overlay
+  inputs.deploy-rs.overlay
+  inputs.agenix.overlays.default
+  inputs.fenix.overlays.default
+  inputs.nur.overlays.default
+  inputs.self.overlays.overrides
+  inputs.self.overlays.emacs
+  inputs.self.overlays.yt-dlp
+]
