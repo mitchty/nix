9bf3f9775762c95be92819fc5955ccfe638ba621
Mitchell Tishmack <git@mitchty.net>
2025-05-17 09:42:33 -0500

    checkpoint: gotta quick copy/pasting so much

--- nix/homeConfigurations/mitch/default.nix
+++ nix/homeConfigurations/mitch/default.nix
@@ -1,4 +1,16 @@
+{ inputs, ... }:
 {
   system = "x86_64-linux";
-  modules = [ ./nuko/nuko.nix ];
+  #  modules = [ inputs.self.homeModules.common ];
+  modules = [
+    {
+      home = {
+        stateVersion = "24.11";
+        username = "mitch";
+        homeDirectory = "/home/mitch";
+        # shellAliases = {
+        #   ls = "eza";
+      };
+    }
+  ];
 }
