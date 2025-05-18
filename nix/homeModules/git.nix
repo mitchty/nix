e62a0cc1bf1fc72297f9dd506e2aa7899f0c6aee
Mitchell Tishmack <git@mitchty.net>
2025-05-18 09:48:34 -0500

    chore: get age kinda working on darwin
    I did an evil to make it work sanely for .netrc though...

--- nix/homeModules/git.nix
+++ nix/homeModules/git.nix
@@ -1,43 +1,56 @@
 {
   pkgs,
+  config,
+  inputs,
   ...
 }:
 {
-  # TODO: need to convert this setup to not directly use .gitconfig files
-  # Programs not (yet) worthy of their own .nix setup... so far who knows what
-  # the future holds.
-  programs.gh = {
-    enable = true;
-    extensions = [
-      pkgs.gh-dash
-      pkgs.gh-cal
-    ];
-  };
-  home = {
-    packages = with pkgs; [
-      transcrypt
-      gist
-      git-absorb
-      git-extras
-      git-lfs
-      git-quick-stats
-      git-recent
-      git-sizer
-      git-vendor
-      gitFull
-    ];
-    file = {
-      # TODO: age encryption need to brain a way to pass that into home
-      # configuration or whatever age encryption is on the things to get working
-      # so I can commit and push to github. Not a huge deal for now I can push
-      # on an existing system until then.
-      #".netrc".source = config.lib.file.mkOutOfStoreSymlink age.secrets."git/netrc".path;
-      #".gh-cli-pub".source = config.lib.file.mkOutOfStoreSymlink age.secrets."git/gh-cli-pub".path;
-      ".gitignore".source = ../../static/git/ignore;
-      ".gitconfig-work".source = ../../static/git/config-work;
-      ".gitconfig".text =
-        (builtins.readFile ../../static/git/config)
-        + (builtins.readFile ../../static/git/config-work-include);
+  imports = [
+    inputs.agenix.homeManagerModules.default
+  ];
+
+  config = {
+    # TODO: need to convert this setup to not directly use .gitconfig files
+    # Programs not (yet) worthy of their own .nix setup... so far who knows what
+    # the future holds.
+    programs.gh = {
+      enable = true;
+      extensions = [
+        pkgs.gh-dash
+        pkgs.gh-cal
+      ];
+    };
+
+    age.secrets."git/netrc".file = ../../secrets/git/netrc.age;
+
+    home = {
+      packages = with pkgs; [
+        transcrypt
+        gist
+        git-absorb
+        git-extras
+        git-lfs
+        git-quick-stats
+        git-recent
+        git-sizer
+        git-vendor
+        gitFull
+      ];
+
+      file = {
+        # NO WORKY
+        #        ".netrc".source = config.lib.file.mkOutOfStoreSymlink config.age.secrets."git/netrc".path;
+
+        # TODO: using config.age.secrets.name.path doesn't work directly ref:
+        # https://github.com/ryantm/agenix/issues/329
+        ".netrc".source = config.lib.file.mkOutOfStoreSymlink "/run/agenix/git/netrc";
+        #".gh-cli-pub".source = config.lib.file.mkOutOfStoreSymlink age.secrets."git/gh-cli-pub".path;
+        ".gitignore".source = ../../static/git/ignore;
+        ".gitconfig-work".source = ../../static/git/config-work;
+        ".gitconfig".text =
+          (builtins.readFile ../../static/git/config)
+          + (builtins.readFile ../../static/git/config-work-include);
+      };
     };
   };
 }
