7fd463626a277de2bf280f995efc988d6579783d
Mitchell Tishmack <git@mitchty.net>
2025-05-21 12:48:32 -0500

    chore: add a script to abuse ffmpeg/ollama to summarize a video file
    Maybe split this into a transcript.sh script and summarize? I dunno future me
    problem.

---
+++ src/summarize.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+#-*-mode: Shell-script; coding: utf-8;-*-
+# Description: abuse abominable intelligence to extract and summarize a video
+# cause videos are long and life is short. Abuses ollama directly cause why not
+# its a script.
+_base=$(basename "$0")
+_dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P || exit 126)
+export _base _dir
+set -e
+
+whisper="${1?need the whisperfile}"
+shift
+video="${1?need a video file too}"
+shift
+model="${1:-llama3.1:latest}"
+
+base=$(TMPDIR=${PREFIX} mktemp -d XXXXXXXX -t)
+
+trap 'rm -fr ${base}' EXIT TERM INT QUIT
+
+cd "${base}" || exit 126
+
+if [ ! -f "${whisper}" ]; then
+  curl -sLO https://huggingface.co/Mozilla/whisperfile/resolve/main/whisper-tiny.en.llamafile
+  install -m755 whisper-tiny.en.llamafile "${whisper}"
+fi
+
+# Extract the audio track and convert to mp3 192k for whisper to use, doesn't
+# seem to work with aac audio.
+ffmpeg -i "${video}" -vn -ar 44100 -ac 2 -b:a 192k output.mp3 > /dev/null 2>&1
+
+${whisper} -pc -f output.mp3 2> /dev/null > whisper.out
+
+cat whisper.out | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g' | awk '/-->/ {gsub(/\\/, "", $0);$1=$2=$3=""; print}' | grep -Ev '\[(MUSIC|SOUND|BLANK_AUDIO)\]' | sed -e 's/^   //' | tee whisper.filtered
+
+{
+  printf "summarize this transcript, only output the summary text don't add any other hints or helpful output about future interaction:\n"
+  cat whisper.filtered
+  # strip the <think> nonsense from reasoning models
+} | tee | ollama run "${model}" | sed -e '/<think>/,/<\/think>/d'
+
+# Iff needed uncomment as necessary.
+#install -m444 whisper.out /tmp
