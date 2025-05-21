bce54172eb3a5e9cbee07b406b3ba081aab0f739
Mitchell Tishmack <git@mitchty.net>
2025-05-21 14:25:15 -0500

    chore: nuke unneeded tee's in favor of coffee from summarize.sh

--- src/summarize.sh
+++ src/summarize.sh
@@ -31,13 +31,13 @@ ffmpeg -i "${video}" -vn -ar 44100 -ac 2 -b:a 192k output.mp3 > /dev/null 2>&1
 
 ${whisper} -pc -f output.mp3 2> /dev/null > whisper.out
 
-cat whisper.out | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g' | awk '/-->/ {gsub(/\\/, "", $0);$1=$2=$3=""; print}' | grep -Ev '\[(MUSIC|SOUND|BLANK_AUDIO)\]' | sed -e 's/^   //' | tee whisper.filtered
+sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g' < whisper.out | awk '/-->/ {gsub(/\\/, "", $0);$1=$2=$3=""; print}' | grep -Ev '\[(MUSIC|SOUND|BLANK_AUDIO)\]' | sed -e 's/^   //' > whisper.filtered
 
 {
   printf "summarize this transcript, only output the summary text don't add any other hints or helpful output about future interaction:\n"
   cat whisper.filtered
   # strip the <think> nonsense from reasoning models
-} | tee | ollama run "${model}" | sed -e '/<think>/,/<\/think>/d'
+} | ollama run "${model}" | sed -e '/<think>/,/<\/think>/d'
 
 # Iff needed uncomment as necessary.
 #install -m444 whisper.out /tmp
