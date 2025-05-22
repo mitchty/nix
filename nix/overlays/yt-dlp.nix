8ffbaa23b1147e706fe51f56974ba7d603867d71
Mitchell Tishmack <git@mitchty.net>
2025-05-22 10:32:17 -0500

    yt-dlp: 2025.5.22

--- nix/overlays/yt-dlp.nix
+++ nix/overlays/yt-dlp.nix
@@ -40,11 +40,11 @@ rec {
   };
   yt-dlp = prev.yt-dlp.overrideAttrs (old: rec {
     latest = "curl --silent https://api.github.com/repos/yt-dlp/yt-dlp/tags | jq -r '.[] | .name' | grep -Ev post | head -n 1 | sed -E 's/\\.0?([1-9])/\\.\\1/g'";
-    version = "2025.4.30";
+    version = "2025.5.22";
     src = prev.fetchPypi {
       inherit version;
       pname = "yt_dlp";
-      hash = "sha256-0BNn0MOulONcseLsy3p8cOGBxMpEj07iN08mSJ0mNgM=";
+      hash = "sha256-6nOFTF2rwSTymjWo+um8XUIu8yMb6+6ivfqCrBkanCk=";
     };
     # postPatch = ''
     #   true
