b16824698a9673c617fa8c59e5836e3cbc335ce5
Mitchell Tishmack <git@mitchty.net>
2025-05-22 18:34:06 -0500

    chore: I am the worst at copy/pasting stuff
    Keep me the hell away from a keyboard today

--- nix/packages/tradingview-mac.nix
+++ nix/packages/tradingview-mac.nix
@@ -12,7 +12,7 @@ if (system == "aarch64-darwin") then
 
     src = fetchurl {
       url = "https://tvd-packages.tradingview.com/stable/${version}/darwin/TradingView.dmg";
-      sha256 = "sha256-i6GNI916WqkYxe1iFiSF48eCoqeEqittmCftrOrSRsI=";
+      sha256 = "sha256-2Gmo0t9VtwBdGUsi23sbSi+FPe+zvDLEMhs9vFyVhH4=";
     };
 
     sourceRoot = ".";
