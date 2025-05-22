720630e6623baf268b4257c0e91a54ea40c5e586
Mitchell Tishmack <git@mitchty.net>
2025-05-22 10:26:02 -0500

    Add a derivation for macos tradingview, maybe override the existing linux one to keep it up to date here too?

---
+++ nix/packages/tradingview-mac.nix
@@ -0,0 +1,47 @@
+{
+  system,
+  stdenv,
+  lib,
+  fetchurl,
+  undmg,
+}:
+if (system == "aarch64-darwin") then
+  stdenv.mkDerivation rec {
+    pname = "tradingview-mac";
+    version = "2.10.0";
+
+    src = fetchurl {
+      url = "https://tvd-packages.tradingview.com/stable/${version}/darwin/TradingView.dmg";
+      sha256 = "";
+    };
+
+    sourceRoot = ".";
+
+    nativeBuildInputs = [
+      undmg
+    ];
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p "$out/Applications"
+      cp -r *.app "$out/Applications"
+
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Tradingview app for macos";
+      platforms = lib.platforms.darwin;
+      sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    };
+
+    latest = "curl --silent https://tvd-packages.tradingview.com/stable/latest/darwin/stable-mac.yml | awk '/version[:]/ {print $2}'";
+  }
+else
+  stdenv.mkDerivation {
+    pname = "tradingview-mac";
+    version = "0.0.0";
+    src = ./.;
+    installPhase = "mkdir -p $out/bin";
+  }
