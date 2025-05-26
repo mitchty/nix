c8467928efa76b3ebd83d83fe88d3b3f12ec09dc
Mitchell Tishmack <git@mitchty.net>
2025-05-26 12:22:04 -0500

    ytdl-sub 2025.05.23

--- nix/packages/ytdl-sub.nix
+++ nix/packages/ytdl-sub.nix
@@ -11,13 +11,13 @@ let
 in
 python3.pkgs.buildPythonApplication rec {
   pname = "ytdl-sub";
-  version = "2025.05.05";
+  version = "2025.05.23";
   format = "pyproject";
 
   src = fetchFromGitHub {
     inherit owner repo;
     rev = "refs/tags/${version}";
-    hash = "sha256-zLYP3iGbTcWuffed7o5RuoYjlUvVFRt8FUOhL1vbW/U=";
+    hash = "sha256-296lizseyBJdpu80MnquPB1ldGgpAcey3iDwiaLqpOQ=";
   };
 
   postPatch = ''
