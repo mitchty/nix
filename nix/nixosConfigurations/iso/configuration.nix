5b9b11a5404ba6e83868dad45059dd1f5e538d00
Mitchell Tishmack <git@mitchty.net>
2025-06-02 08:41:35 -0500

    chore: get the rescue iso image to be more useful

--- nix/nixosConfigurations/iso/configuration.nix
+++ nix/nixosConfigurations/iso/configuration.nix
@@ -6,6 +6,11 @@
   inputs,
   ...
 }:
+let
+  sshPubKeys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCl1r2eksJXO02QkuGbjVly38MhG9MpDfvQRPABWJLGfFIBQFNkCvvJffV1UEUpcRNNaAmle1DFS1CtvATZSr/UpTgzsAYu9X+gd0/5OB/WlWHJaC/j0H2LahtiUPKZ2d4/cLkKPQqP6HZdmOXrsHZR1I9bxjhqyNWhwxNLMCK/8995hKNWOYamMagJloHUTRLFQaor/WoFDqjfW8EKo09OxKnXtFFcj6CmXwsu1RWfFY/P/wsADL+8B2/P4CmqqwuLxQknbA0WZ2zWSj13tf24H7BORAkMAeK5249GuLd5SlnnvmHJLiF1OCIkSOZJMcyrNCCvBRavGLcPoKQbtHw7"
+  ];
+in
 {
   imports = [
     "${toString inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
@@ -18,4 +23,7 @@
     ];
     variables.EDITOR = "vi";
   };
+
+  users.users.root.openssh.authorizedKeys.keys = sshPubKeys;
+  isoImage.squashfsCompression = lib.mkForce "zstd -Xcompression-level 19";
 }
