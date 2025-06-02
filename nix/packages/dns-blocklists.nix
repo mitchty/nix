b508c7c9485933f1e201db575a3a4e220ff9a764
Mitchell Tishmack <git@mitchty.net>
2025-06-02 13:20:02 -0500

    dns-blocklists 32025.153.61552

--- nix/packages/dns-blocklists.nix
+++ nix/packages/dns-blocklists.nix
@@ -10,12 +10,12 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "dns-blocklists";
-  version = "32025.153.11798";
+  version = "32025.153.61552";
 
   src = pkgs.fetchFromGitHub {
     inherit owner repo;
     rev = "${version}";
-    sha256 = "sha256-1Hj2rKtNgHDN8/IEulodNsMYJxOGUjWmLijRM04bKTw=";
+    sha256 = "sha256-/1bZ3r4wsQ6AO2MFQz0lyDf7rTsXInYvedCMFi59lSw=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
