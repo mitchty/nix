032fd5b22ee1492e330092d1a338de7720d11e8f
Mitchell Tishmack <git@mitchty.net>
2025-06-02 08:08:28 -0500

    chore: update the iso nixosConfiguration will be abusing that tonight

--- nix/nixosConfigurations/iso/configuration.nix
+++ nix/nixosConfigurations/iso/configuration.nix
@@ -1,3 +1,4 @@
+# Build me with nix build .#nixosConfigurations.iso.config.system.build.isoImage
 {
   config,
   lib,
@@ -6,5 +7,15 @@
   ...
 }:
 {
-  environment.variables.EDITOR = "vi";
+  imports = [
+    "${toString inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+  ] ++ (with inputs.self.nixosModules; [ install-iso ]);
+
+  environment = {
+    systemPackages = [
+      pkgs.disko
+      pkgs.home-manager
+    ];
+    variables.EDITOR = "vi";
+  };
 }
