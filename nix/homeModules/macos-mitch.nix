17b82101dc306f0c6461e8792450a795eb9390e2
Mitchell Tishmack <git@mitchty.net>
2025-06-07 08:34:37 -0500

    macos-mitch: add utm and ytdl stuff to macos by default
    I'll fix this in post later but was lazy

--- nix/homeModules/macos-mitch.nix
+++ nix/homeModules/macos-mitch.nix
@@ -10,13 +10,10 @@
   ];
 
   home = {
-    # sessionVariables = {
-    #   BROWSER = "librewolf";
-    #   GDK_DPI_SCALE = "1.25";
-    #   QT_SCALE_FACTOR = "1.25";
-    # };
     packages = with pkgs; [
-      gron
+      utm
+      ytdl-sub
+      yt-dlp
     ];
   };
 }
