b0943ed784dd54c96a09209a51de4d430e2f26c5
Mitchell Tishmack <git@mitchty.net>
2025-06-08 08:40:50 -0500

    src/cdu.sh: rewrite to use cargo metadata to get bin targets
    Should be more reliable/portable this way. Also maybe I just set a global
    CARGO_TARGET_DIR for things I forget to change it to.

--- src/cdu.sh
+++ src/cdu.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env sh
 #-*-mode: Shell-script; coding: utf-8;-*-
-# Description: For quick printing out the size of binaries
+# Description: For quick printing out the size of cargo binaries via like hwatch
+# or whatever cause... god knows its more so I can see the impact of adding deps
+# on binary size.
 _base=$(basename "$0")
 _dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P || exit 126)
 export _base _dir
 
-(for x in debug release; do
-  files=$(find "${CARGO_TARGET_DIR:-target}/${x}" -maxdepth 1 -type f \( ! -name "*.d" -a ! -name "*.rlib" -a ! -name ".cargo-lock" \))
-  du -hs "${files}"
-done) | sed -e "s|/.*\(release\)/|\1/|" -e "s|/.*\(debug\)/|\1/|" | sort -k2
+# Dump out from cargo all the binary targets in the workspace (or not as it may
+# be) then just pipe that crap to sed to strip out everything in front of
+# release/debug and the binary name itself so we don't have gihugic lines and
+# get like size {debug,release}/bin
+(for bin in $(cargo metadata --format-version 1 --no-deps | jq -r '.packages[].targets[] | select(.kind[] == "bin") | .name'); do
+  find "${CARGO_TARGET_DIR}" -type f -name "${bin}" -exec du -hs {} \+
+done) | sed -e "s,/.*\/\(release\|debug\)/\(.*\)$,\1/\2," | sort -k2
