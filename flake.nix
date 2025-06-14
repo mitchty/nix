e0be923ef37f8cafb3f348e31f72522e77363bf6
Mitchell Tishmack <git@mitchty.net>
2025-06-14 11:46:41 -0500

    chore: add and doc some details on searching nix flake input packages
    Took a while to figure out and I wanted to compare the version numbers between
    inputs/branches of nixpkgs.

--- flake.nix
+++ flake.nix
@@ -82,11 +82,13 @@
     # Release YY.MM branch name stuff kept close together for lazy.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-old.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # TODO: do I really need this?
+    # TODO: do I really need this anymore? here for future me to uncomment if I
+    # do.
     #    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
     nix-darwin = {
       url = "github:LnL7/nix-darwin/nix-darwin-25.05";
