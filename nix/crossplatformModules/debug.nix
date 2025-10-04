39ac8e66c18ae8173f0554b05fca48566813e042
Mitchell Tishmack <git@mitchty.net>
2025-10-04 09:15:24 -0500

    chore: migrate debug packages to crossplatform modules

--- nix/crossplatformModules/debug.nix
+++ nix/crossplatformModules/debug.nix
@@ -1,7 +1,15 @@
-{ lib, ... }:
+{ pkgs, ... }:
 
 {
   # For debugging purposes keep the current flake used to construct things into
   # /etc
-  environment.etc."current-flake".source = ./../..;
+  environment = {
+    etc."current-flake".source = ./../..;
+
+    systemPackages = with pkgs; [
+      tcpdump
+      pssh
+      nix-sweep
+    ];
+  };
 }
--- nix/nixosModules/debug.nix
+++ nix/nixosModules/debug.nix
@@ -5,7 +5,6 @@
 {
   environment.systemPackages = with pkgs; [
     pciutils
-    tcpdump
     mcelog
   ];
 }
