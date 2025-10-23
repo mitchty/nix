458add96605b3500e86e621e45f46108ff32125c
Mitchell Tishmack <git@mitchty.net>
2025-10-22 22:29:03 -0500

    tradingview-mac 2.14.0

--- nix/packages/tradingview-mac.nix
+++ nix/packages/tradingview-mac.nix
@@ -9,11 +9,11 @@
 if (system == "aarch64-darwin") then
   pkgs.stdenvNoCC.mkDerivation rec {
     pname = "tradingview-mac";
-    version = "2.13.0";
+    version = "2.14.0";
 
     src = fetchurl {
       url = "https://tvd-packages.tradingview.com/stable/${version}/darwin/TradingView.dmg";
-      sha256 = "sha256-/aWCfmaabybatPkbJaj1a9wPbVvYLfBEtTjJJbvD2eE=";
+      sha256 = "sha256-6SuE0WT0bdZVjsDYs4ttB6ReW1jar7qGqWiLvH/DfN8=";
     };
 
     sourceRoot = ".";
